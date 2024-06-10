5f7b8e8331d24ab0b97e6d8675fcd1406c0a0861
MRensen <markrensen@hotmail.com>
2024-06-10 10:13:49 +0200

    Added token reset script

---
+++ myscripts/authreset.ps1
@@ -0,0 +1,5 @@
+#version 1
+
+$content = Get-Content ./*postman_collection.json -Raw
+>> $modifiedContent = $content -replace '"auth": \{[^}]*\}\s+]\s+\},', ''
+>> Set-Content ./*postman_collection.json -Value $modifiedContent
