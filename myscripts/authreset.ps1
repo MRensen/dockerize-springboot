fc40b2c003d099def5f6dc53f859ee8ed8fe5ddd
MRensen <markrensen@hotmail.com>
2024-06-10 10:32:35 +0200

    fixed type in authreset and delete old readme

--- myscripts/authreset.ps1
+++ myscripts/authreset.ps1
@@ -1,5 +1,5 @@
 #version 1
 
 $content = Get-Content ./*postman_collection.json -Raw
->> $modifiedContent = $content -replace '"auth": \{[^}]*\}\s+]\s+\},', ''
->> Set-Content ./*postman_collection.json -Value $modifiedContent
+$modifiedContent = $content -replace '"auth": \{[^}]*\}\s+]\s+\},', ''
+Set-Content ./*postman_collection.json -Value $modifiedContent
