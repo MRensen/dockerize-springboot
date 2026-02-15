63b9eb1ac675abc958a43f1ff83276933d29eda6
MRensen <markrensen@hotmail.com>
2026-02-15 12:26:11 +0100

    kc automatisering toegevoegd

--- myscripts/dockerize.ps1
+++ myscripts/dockerize.ps1
@@ -36,6 +36,9 @@ Param(
     
     [Parameter(Mandatory=$false)]
     [string]$sql = "postgresql" # Optional flag with a default value
+
+    [Parameter(Mandatory=$false)]
+    [switch]$kc
 )
 
 #Deze twee bewerkingen worden nu in de Dockerfile gedaan
@@ -67,6 +70,37 @@ else{
 	"PostgreSql"
 	Copy-Item 'C:\Windows\System32\WindowsPowerShell\v1.0\MyScripts\postgresql-compose\docker-compose.yml' -Destination .
 }
+
+if($kc)
+{
+    "Using kc"
+    $rootDir = Get-Location
+    $resourcesDir = "$rootDir/src/main/resources"
+
+    # Functie om realm-bestanden te kopiëren naar resources directory
+    function Copy-RealmFiles {
+        param (
+            [string]$sourceDir
+        )
+
+        # Haal realm bestanden in de opgegeven directory
+        $realmFiles = Get-ChildItem -Path $sourceDir -Filter '*.json' | Where-Object { $_.Name -match 'realm' }
+
+        foreach ($file in $realmFiles) {
+            # Kopieer elk bestand naar de resources map
+            Copy-Item -Path $file.FullName -Destination $resourcesDir -Force
+        }
+    }
+
+    # Kopieer realm-bestanden vanuit het root project directory
+    Copy-RealmFiles -sourceDir $rootDir
+
+    # Kopieer realm-bestanden vanuit de bovenliggende directory
+    $parentDir = (Get-Item $rootDir).Parent.FullName
+    Copy-RealmFiles -sourceDir $parentDir
+}
+
+
 "Buidling with 'docker build -t'..."
 docker build -t image .
 "Starting with 'docker compose up -d'..."
