676c7904b87d8597f644fede88d4f5e7d3bcd176
MRensen <markrensen@hotmail.com>
2026-02-17 21:04:39 +0100

    update met keycloak en jdk25

--- myscripts/dockerize.ps1
+++ myscripts/dockerize.ps1
@@ -35,7 +35,7 @@ Param(
     [int]$jdk = 17,
     
     [Parameter(Mandatory=$false)]
-    [string]$sql = "postgresql" # Optional flag with a default value
+    [string]$sql = "postgresql", # Optional flag with a default value
 
     [Parameter(Mandatory=$false)]
     [switch]$kc
@@ -54,6 +54,11 @@ if($jdk -eq 21)
 	"JDK 21"
 	Copy-Item 'C:\Windows\System32\WindowsPowerShell\v1.0\MyScripts\docker-jdk21\Dockerfile' -Destination .
 }
+elseif($jdk -eq 25)
+{
+	"JDK 25"
+	Copy-Item 'C:\Windows\System32\WindowsPowerShell\v1.0\MyScripts\docker-jdk25\Dockerfile' -Destination .
+}
 else
 {
 	"JDK 17"
@@ -71,37 +76,80 @@ else{
 	Copy-Item 'C:\Windows\System32\WindowsPowerShell\v1.0\MyScripts\postgresql-compose\docker-compose.yml' -Destination .
 }
 
+$rootDir = Get-Location
+$resourcesDir = "$rootDir/src/main/resources"
+$kcDir = "$rootDir/kc"
+
+ # Controleer of de kc directory bestaat, zo niet, maak deze aan.
+if (-Not (Test-Path -Path $kcDir)) {
+    Write-Host "kc directory bestaat niet, wordt aangemaakt..."
+    New-Item -Path $kcDir -ItemType Directory | Out-Null
+}
+
 if($kc)
 {
     "Using kc"
-    $rootDir = Get-Location
-    $resourcesDir = "$rootDir/src/main/resources"
 
     # Functie om realm-bestanden te kopiëren naar resources directory
+#    function Copy-RealmFiles {
+#        param (
+#            [string]$sourceDir
+#        )
+#
+#        # Haal realm bestanden in de opgegeven directory
+#        $realmFiles = Get-ChildItem -Path $sourceDir -Filter '*.json' | Where-Object { $_.Name -match 'realm' }
+#
+#        foreach ($file in $realmFiles) {
+#            # Lees het bestand en vervang tekst
+#            $content = Get-Content -Path $file.FullName -Raw
+#            $content = $content.Replace("http://localhost:9090", "http://kc:9090")
+#
+#            # Bepaal bestemmingspad en schrijf het gewijzigde bestand
+#            $destinationFile = Join-Path -Path $kcDir -ChildPath $file.Name
+#            $content | Set-Content -Path $destinationFile
+#
+#            Write-Host "Aangepast en gekopieerd: $destinationFile"
+#        }
+#    }
     function Copy-RealmFiles {
         param (
             [string]$sourceDir
         )
-
         # Haal realm bestanden in de opgegeven directory
         $realmFiles = Get-ChildItem -Path $sourceDir -Filter '*.json' | Where-Object { $_.Name -match 'realm' }
-
         foreach ($file in $realmFiles) {
             # Kopieer elk bestand naar de resources map
-            Copy-Item -Path $file.FullName -Destination $resourcesDir -Force
+            Copy-Item -Path $file.FullName -Destination $kcDir -Force
         }
     }
 
-    # Kopieer realm-bestanden vanuit het root project directory
-    Copy-RealmFiles -sourceDir $rootDir
+    # Check of er al een realm-bestand bestaat in de resources directory
+    $existingRealmFiles = Get-ChildItem -Path $kcDir -Filter '*.json' | Where-Object { $_.Name -match 'realm' }
 
-    # Kopieer realm-bestanden vanuit de bovenliggende directory
-    $parentDir = (Get-Item $rootDir).Parent.FullName
-    Copy-RealmFiles -sourceDir $parentDir
+    if ($existingRealmFiles.Count -gt 0) {
+        Write-Host "Er bestaat al een realm-bestand in de kc directory. Kopieeractie wordt overgeslagen."
+    } else {
+        # Kopieer realm-bestanden vanuit het root project directory
+        Copy-RealmFiles -sourceDir $rootDir
+
+        # Kopieer realm-bestanden vanuit de resources map
+        Copy-RealmFiles -sourceDir $resourcesDir
+
+        # Kopieer realm-bestanden vanuit de bovenliggende directory
+        $parentDir = (Get-Item $rootDir).Parent.FullName
+        Copy-RealmFiles -sourceDir $parentDir
+    }
+    # Copy entrypoint script
+    Copy-Item 'C:\Windows\System32\WindowsPowerShell\v1.0\MyScripts\entrypoint.sh' -Destination .
+} else {
+    # Copy entrypoint script
+    Copy-Item 'C:\Windows\System32\WindowsPowerShell\v1.0\MyScripts\entrypoint-plain.sh' -Destination '.\entrypoint.sh'
 }
 
 
-"Buidling with 'docker build -t'..."
+
+
+"Building with 'docker build -t'..."
 docker build -t image .
 "Starting with 'docker compose up -d'..."
 docker compose up -d
